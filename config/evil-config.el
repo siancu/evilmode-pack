6aec132a41681f790a568ca53ed846c2f39d58cd
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-13 00:03:14 +0100

    Initial commit for the evilmode-pack.

---
+++ config/evil-config.el
@@ -0,0 +1,7 @@
+;; Evil-mode configuration
+
+;; Add evil-mode to the load path
+(live-add-pack-lib "evil")
+
+(require 'evil)
+(evil-mode 1)
---
+++ init.el
@@ -0,0 +1,4 @@
+;; Evil-mode pack init file
+;;
+
+(live-load-config-file "evil-config.el")
