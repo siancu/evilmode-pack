ff491bc5998c8d1c59b40405a4fe917762b5baeb
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-23 10:59:09 +0100

    Fixed setting evil-leader to ",".

--- config/evil-leader-conf.el
+++ config/evil-leader-conf.el
@@ -2,7 +2,7 @@
 
 (live-add-pack-lib "evil-leader")
 
-(require 'evil-leader)
-
 ;; set the leader key to comma
 (setq evil-leader/leader "," evil-leader/in-all-states t)
+
+(require 'evil-leader)
--- init.el
+++ init.el
@@ -4,11 +4,11 @@
 ;; evil
 (live-load-config-file "evil-conf.el")
 
-;; evil-surround
-(live-load-config-file "evil-surround-conf.el")
-
 ;; evil-leader
 (live-load-config-file "evil-leader-conf.el")
+
+;; evil-surround
+(live-load-config-file "evil-surround-conf.el")
 
 ;; evil-nerd-commenter
 (live-load-config-file "evil-nerd-commenter-conf.el")
